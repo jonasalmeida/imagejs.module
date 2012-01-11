f2e1af8b36fbb5e26978eb6f9d112a426eb73dfc
Jonas Almeida <jalmeida@mathbiol.org>
2012-01-10 23:01:23 -0600

    using jmat.plot

--- morphomarkers.js
+++ morphomarkers.js
@@ -11,21 +11,27 @@
 	var url='morphomarkers.js';
 	imagejs.modules[url]={
 		dist:function(dt,px){ //distance between image data and a pixel
-			//dt.ma
+			if(px.length==2){px=dt[px[0]][px[1]]} // in case the pixel coordinates rather than the pixel is being submitted as px
+			//console.log(px);
+			return jmat.imMap(dt,function(xy){
+				// Euclidean distance 
+				// notice how elegantly the px pixel value is passed to the function through a closure :-)
+				return Math.pow(jmat.sum(xy.slice(0,3).map(function(xyi,i){return Math.pow((xyi-px[i]),2)})),1/2);
+			})	
 		},
 		start:function(){
-			jmat.gId('cvBase').onclick=function(evt){
+			jmat.gId('cvTop').onclick=function(evt){ // click on top for things hapenning in cvBase
 				//imagejs.msg('Morphomarker acquisition ...');
 				var x = evt.clientX-evt.target.offsetLeft;
 				var y = evt.clientY-evt.target.offsetTop;
 				//console.log(x,y);
 				imagejs.msg('('+x+','+y+')');
-				var dt = jmat.cloneArray(imagejs.data.dt0);
-				//var yx=dt[y][x];
-				if (jmat.max(dt[y][x].slice(0,3))>150){dt[y][x]=[0,0,0,dt[y][x][3]]}
-				else{dt[y][x]=[255,255,255,dt[y][x][3]]}
-				jmat.imwrite(this,dt);
-				//imagejs.msg('acquiring ('+x+','+y+') done ');
+				//var dt = jmat.cloneArray(imagejs.data.dt0);
+				if (jmat.max(imagejs.data.dt0[y][x].slice(0,3))>150){var C=[0,0,1]}
+				else{var C=[1,1,0]}
+				var ctx=this.getContext('2d');
+				ctx.clearRect(0,0,this.width,this.height);
+				jmat.plot(this,x,y,'+',{Color:C,MarkerSize:30})
 			}
 		},
 		end:function(){jmat.gId('cvBase').onclick=null}
