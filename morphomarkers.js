0a8fb02db98130787a2f2a97517605bb4ba14673
Jonas Almeida <almeida.jonas@gmail.com>
2012-02-23 15:53:18 -0600

    activated "New" checkbox, image.data use simplified to include only .seg (segmented) attribute

--- morphomarkers.js
+++ morphomarkers.js
@@ -20,6 +20,8 @@
 				return Math.pow(jmat.sum(xy.slice(0,3).map(function(xyi,i){return Math.pow((xyi-px[i])*Wrgba[i],2)})),1/2);
 			})	
 		},
+		segCheckOnChange:function(that){imagejs.modules[id].segNewChecked=that.checked},
+		segNewChecked:true,
 		start:function(){
 			cvTop.style.cursor='crosshair';
 			cvTop.style.left=cvBase.offsetLeft;cvTop.style.top=cvBase.offsetTop; // make sure they're aligned
@@ -37,12 +39,22 @@
 				if(!imagejs.modules[id].red){imagejs.modules[id].red=100};
 				if(!imagejs.modules[id].green){imagejs.modules[id].green=100};
 				if(!imagejs.modules[id].blue){imagejs.modules[id].blue=100};
-				if(!imagejs.modules[id].d){var d = imagejs.modules[id].dist(imagejs.data.dt0,[y,x]);imagejs.data.d=d;}
-				else {var d = imagejs.modules[id].d}
-				if(!imagejs.modules[id].thr){var thr = jmat.max(jmat.max(d))/5;imagejs.modules[id].thr=thr;}
+				if(!imagejs.modules[id].d){imagejs.modules[id].d=imagejs.modules[id].dist(imagejs.data.dt0,[y,x])}
+				//if(!imagejs.modules[id].d){var d = imagejs.modules[id].dist(imagejs.data.dt0,[y,x]);imagejs.modules[id].d=d;}
+				//else {var d = imagejs.modules[id].d}
+				if(!imagejs.modules[id].thr){var thr = jmat.max(jmat.max(imagejs.modules[id].d))/5;imagejs.modules[id].thr=thr;}
 				else {var thr = imagejs.modules[id].thr}
-				var bw = jmat.im2bw(d,thr); // threshold should be recoded to allow for a function
+				var bw = jmat.im2bw(imagejs.modules[id].d,thr); // threshold should be recoded to allow for a function
 				var bw = jmat.arrayfun(bw,function(x){return 1-x}); // get the reciprocal
+				if(!imagejs.modules[id].segOld){imagejs.modules[id].segOld=bw}
+				if(!imagejs.data.seg){imagejs.data.seg=bw}
+				//if(!imagejs.modules[id].segNewChecked){imagejs.modules[id].segNewChecked=true} // default value
+				if(!imagejs.modules[id].segNewChecked){
+					bw=jmat.innerfun(bw,imagejs.data.seg,function(a,b){if(a==1){return a}else{return b}});
+					imagejs.modules[id].segNewChecked=false;
+				}; // add to previous segmentation
+				//else{imagejs.modules[id].segNewChecked=true}; // default value
+				imagejs.modules[id].segOld=imagejs.data.seg;imagejs.data.seg=bw;
 				//jmat.imagesc(cvTop,bw); // display it
 				//jmat.imagebw(cvTop,bw,[0,0,0,0],[255,255,0,255]); // display segmentation
 				var edg = jmat.edge(bw);
@@ -50,10 +62,10 @@
 				//var C=[1,1,0]; // always use yellow
 				//jmat.plot(cvTop,x,y,'+',{Color:C,MarkerSize:30});
 				//jmat.plot(cvTop,x,y,'o',{Color:C,MarkerSize:30});
-				
-				msg.innerHTML='[x]new <button><</button> Threshold: <span id="slider">___|___|___|___|___|___|___|___|___|___</span> . <span id="sliderRed" style="color:red">__|__|__|__|__</span> . <span id="sliderGreen" style="color:green">__|__|__|__|__</span> . <span id="sliderBlue" style="color:blue">__|__|__|__|__</span>';
+				msg.innerHTML='<input id="segNew" type="checkbox" onchange="imagejs.modules[\''+id+'\'].segCheckOnChange(this)">New <button><</button> Threshold: <span id="slider">___|___|___|___|___|___|___|___|___|___</span> . <span id="sliderRed" style="color:red">__|__|__|__|__</span> . <span id="sliderGreen" style="color:green">__|__|__|__|__</span> . <span id="sliderBlue" style="color:blue">__|__|__|__|__</span>';
+				$('#segNew').attr('checked',imagejs.modules[id].segNewChecked);
 				$(function(){$('#slider').slider({
-					max:jmat.max(jmat.max(d)),
+					max:jmat.max(jmat.max(imagejs.modules[id].d)),
 					min:0,
 					value:thr,
 					change:function(){imagejs.modules[id].thr=$('#slider').slider('value');jmat.gId('cvTop').onclick(evt,x,y)}
@@ -87,7 +99,9 @@
 				jmat.plot(cvTop,x,y,'o',{Color:C,MarkerSize:30});
 				cvTop.style.left=cvBase.offsetLeft;cvTop.style.top=cvBase.offsetTop; // make sure the two canvas are aligned
 				//cvTopOnClick(evt);
-				setTimeout(cvTopOnClick, 20, evt);				
+				setTimeout(cvTopOnClick, 40, evt);
+				if(!!imagejs.modules[id].d){delete imagejs.modules[id].d}
+				//imagejs.modules[id].d=imagejs.modules[id].dist(imagejs.data.dt0,[y,x]);				
 			}
 		},
 		end:function(){
