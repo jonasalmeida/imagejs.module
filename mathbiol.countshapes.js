d8bc8435be4e3760ed1152b7e4e1adbf293f0377
Jonas Almeida <almeida.jonas@gmail.com>
2012-02-28 14:55:53 -0600

    menu added to mathbiol.countshapes

--- mathbiol.countshapes.js
+++ mathbiol.countshapes.js
@@ -1,2 +1,22 @@
 // ImageJS module to count shapes
-console.log('shapecount loaded :-)')
+console.log('shapecount library loaded');
+// write module as a function call to avoid messing global scope
+
+(function(){
+	
+	var ShapesMenu={
+		Start:function(){
+			console.log('Load Module');
+			msg.innerHTML='Counting started ...';
+			cvTop.style.left=cvBase.offsetLeft;cvTop.style.top=cvBase.offsetTop; // make sure the two canvas are aligned
+		},
+		End:function(){
+			console.log('... counting ended.');
+		}
+	}
+	jmat.gId('menu').appendChild(imagejs.menu(ShapesMenu,'CountShapes'))
+	
+})()
+
+
+
