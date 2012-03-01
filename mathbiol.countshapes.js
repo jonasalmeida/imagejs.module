99ab46d7e5443e777beab71a8450765ffebfad4f
Jonas Almeida <almeida.jonas@gmail.com>
2012-03-01 12:33:10 -0600

    intermediate commit to explain Viajaya  how to trigger methods

--- mathbiol.countshapes.js
+++ mathbiol.countshapes.js
@@ -4,16 +4,31 @@ console.log('shapecount library loaded');
 
 (function(){
 	
+	
+	var id='countshapes'; // name of the modules attribute where module-specific stuff will be stored
+	imagejs.modules[id]={ // this way all that pertains to the inner workings of this module stays in this branch
+		start:function(){
+			
+		},
+		end:function(){
+			
+		}
+		
+	}
+	
+	// Assemble CountShapes menu
 	var ShapesMenu={
 		Start:function(){
-			msg.innerHTML='Counting started ...';
+			imagejs.msg('Counting started ...');
+			imagejs.modules[id].start();
 			cvTop.style.left=cvBase.offsetLeft;cvTop.style.top=cvBase.offsetTop; // make sure the two canvas are aligned
 		},
 		End:function(){
-			console.log('... counting ended.');
+			imagejs.msg('... counting ended.');
+			imagejs.modules[id].end();
 		}
 	}
-	jmat.gId('menu').appendChild(imagejs.menu(ShapesMenu,'CountShapes')); //assemble meny
+	jmat.gId('menu').appendChild(imagejs.menu(ShapesMenu,'CountShapes')); //assemble menu
 	
 })()
 
