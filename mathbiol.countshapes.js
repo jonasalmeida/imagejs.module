2016f4975dbe3c63990dd1747fcaddad0ca30bfe
Jonas Almeida <almeida.jonas@gmail.com>
2012-03-02 01:43:40 -0600

    density counted

--- mathbiol.countshapes.js
+++ mathbiol.countshapes.js
@@ -8,12 +8,44 @@ console.log('shapecount library loaded');
 	var id='countshapes'; // name of the modules attribute where module-specific stuff will be stored
 	imagejs.modules[id]={ // this way all that pertains to the inner workings of this module stays in this branch
 		start:function(){
+			var divCountShapes=this.createCountDiv();
+			imagejs.modules[id].currentDivId=divCountShapes.id;// current count div
+			imagejs.modules[id][divCountShapes.id]={}; // save count results here
+			this.count(divCountShapes.id); // action
+			
 			
 		},
 		end:function(){
 			
+		},
+		alignCanvas:function(){
+			if($('#cvTop').length>0){
+				cvTop.style.left=cvBase.offsetLeft;cvTop.style.top=cvBase.offsetTop // make sure the two canvas are aligned
+			}
+		},
+		createCountDiv:function(){
+			var divCountShapes=$('<div>')[0];
+			divCountShapes.id=jmat.uid('divCountShapes');
+			//divCountShapes.style.width=200;
+			$(divCountShapes).addClass("ui-widget-content");
+			var H='<button style="color:red" onclick="this.parentElement.parentElement.removeChild(this.parentElement)">[x]</button>';
+			H+=' | <button style="color:green" onclick="lala=this;console.log(this)">[>]</button>'
+			H+='<table>';
+			H+='<tr><td>Circularity:</td><td class="countShapesCircularity">...</td></tr>';
+			H+='<tr><td>Intensity:</td><td class="countShapesIntensity">...</td></tr>';
+			H+='<tr><td>Density:</td><td class="countShapesDensity" style="color:blue">...</td></tr>';
+			H+='<tr><td>Features:</td><td id="countShapesFeatures">...</td></tr>';
+			H+='</table>';
+			divCountShapes.innerHTML = H;
+			menu.appendChild(divCountShapes);
+			return divCountShapes;
+		},
+		count:function(divId){
+			console.log(divId);
+			imagejs.modules[id][divId].density = jmat.sum(jmat.sum(imagejs.data.seg))/jmat.prod(jmat.size(imagejs.data.seg));
+			$('#'+divId+' .countShapesDensity').html(imagejs.modules[id][divId].density*100 + ' %');
+			
 		}
-		
 	}
 	
 	// Assemble CountShapes menu
@@ -21,7 +53,7 @@ console.log('shapecount library loaded');
 		Start:function(){
 			imagejs.msg('Counting started ...');
 			imagejs.modules[id].start();
-			cvTop.style.left=cvBase.offsetLeft;cvTop.style.top=cvBase.offsetTop; // make sure the two canvas are aligned
+			imagejs.modules[id].alignCanvas(); 
 		},
 		End:function(){
 			imagejs.msg('... counting ended.');
