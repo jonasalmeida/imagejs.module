5a3045667eeaaa44ce9b5475a91b0b2568465188
Jonas Almeida <almeida.jonas@gmail.com>
2012-03-12 16:36:50 -0500

    separation of segmentation and feature statistics

--- mathbiol.countshapes.js
+++ mathbiol.countshapes.js
@@ -11,9 +11,8 @@ console.log('shapecount library loaded');
 			var divCountShapes=this.createCountDiv();
 			imagejs.modules[id].currentDivId=divCountShapes.id;// current count div
 			imagejs.modules[id][divCountShapes.id]={}; // save count results here
-			this.fillDiv(divCountShapes.id); // action
-			
-			
+			this.segmentationStats(divCountShapes.id); // action
+			this.alignCanvas();
 		},
 		end:function(){
 			
@@ -28,33 +27,76 @@ console.log('shapecount library loaded');
 			divCountShapes.id=jmat.uid('divCountShapes');
 			//divCountShapes.style.width=200;
 			$(divCountShapes).addClass("ui-widget-content");
-			var H='<button style="color:red" onclick="this.parentElement.parentElement.removeChild(this.parentElement);imagejs.modules.'+id+'.alignCanvas()">[x]</button>';
-			H+=' | <button style="color:green" onclick="imagejs.modules.'+id+'.fillDiv(\''+divCountShapes.id+'\')">[>]</button>'
-			H+='<table>';
-			H+='<tr><td>File:</td><td class="countShapesFile" style="color:blue">...</td><td>Density:</td><td class="countShapesDensity" style="color:blue">...</td></tr>';
-			H+='</table>';
+			var H='';
+			// First line, file name and close button
+			H+='<button style="color:red" onclick="this.parentElement.parentElement.removeChild(this.parentElement);imagejs.modules.'+id+'.alignCanvas()">[x]</button>';
+			H+=' File :<span class="countShapesFile" style="color:blue"></span>';
+			H+=' .<br>';
+			// Second line, Segmentation Statistics
+			H+='<button style="color:green" onclick="imagejs.modules.'+id+'.segmentationStats(\''+divCountShapes.id+'\')">Segmentation</button>';
+			H+=' Density <span style="color:blue" class="countShapesDensity"> ... </span>';
+			H+=' Intensity <span style="color:blue" class="countShapesIntensity"> ... </span>';
+			H+=' .<br>';
+			//H+='<table>';
+			//H+='<tr><td>File:</td><td class="countShapesFile" style="color:blue">...</td><td>Density:</td><td class="countShapesDensity" style="color:blue">...</td></tr>';
+			//H+='</table>';
 			//H+='<tr><td>Circularity:</td><td class="countShapesCircularity">...</td></tr>';
 			//H+='<tr><td>Intensity:</td><td class="countShapesIntensity">...</td></tr>';
 			//H+='<tr><td>Density:</td><td class="countShapesDensity" style="color:blue">...</td></tr>';
-			H+='<table>';
-			H+='<tr><td><button onclick="imagejs.modules.'+id+'.featuresTable(\''+divCountShapes.id+'\')">Features</button> number:</td><td class="countShapesFeatures" style="color:blue">...</td></tr>';
-			H+='</table>';
+			//H+='<table>';
+			//H+='<tr><td><button onclick="imagejs.modules.'+id+'.featuresTable(\''+divCountShapes.id+'\')">Features</button> Number:</td><td class="countShapesFeatures" style="color:blue">...</td></tr>';
+			//H+='</table>';
+			H+='<button onclick="imagejs.modules.'+id+'.featuresStats(\''+divCountShapes.id+'\')" style="color:green">Features</button> Number: <span class="countShapesFeatures" style="color:blue">...</span>'
 			divCountShapes.innerHTML = H;
 			menu.appendChild(divCountShapes);
 			return divCountShapes;
 		},
-		fillDiv:function(divId){
+		segmentationStats:function(divId){
 			console.log(divId);
 			$('#'+divId+' .countShapesFile').html(imagejs.data.fname);
-			imagejs.data.density = jmat.sum(jmat.sum(imagejs.data.seg))/jmat.prod(jmat.size(imagejs.data.seg));
+			imagejs.data.size=jmat.size(imagejs.data.seg);
+			imagejs.data.n=jmat.prod(imagejs.data.size);
+			imagejs.data.density = jmat.sum(jmat.sum(imagejs.data.seg))/imagejs.data.n;
 			var d = imagejs.data.density*100+'';if(d.length>6){d=d.slice(0,6)};
 			$('#'+divId+' .countShapesDensity').html(d + ' %');
+			//imagejs.data.IrgbTotal=imagejs.data.dt0.map(function(x){
+			//	return x.map(function(y){return y}).reduce(function(a,b){
+			//		return [a[0]+b[0],a[1]+b[1],a[2]+b[2]]
+			//		})
+			//	}).reduce(function(a,b){
+			//	return [a[0]+b[0],a[1]+b[1],a[2]+b[2]]
+			//});
+			//imagejs.data.IrgbTotal.map(function(x){return x/imagejs.data.n});
+			imagejs.data.IrgbSeg=imagejs.data.dt0.map(function(x,i){
+				return x.map(function(y,j){
+						if (imagejs.data.seg[i][j]>0){return y}
+						else {return [0,0,0]}
+					}).reduce(function(a,b){
+					return [a[0]+b[0],a[1]+b[1],a[2]+b[2]]
+					})
+				}).reduce(function(a,b){
+				return [a[0]+b[0],a[1]+b[1],a[2]+b[2]]
+			});
+			imagejs.data.IrgbNonSeg=imagejs.data.dt0.map(function(x,i){
+				return x.map(function(y,j){
+						if (imagejs.data.seg[i][j]>0){return [0,0,0]}
+						else {return y}
+					}).reduce(function(a,b){
+					return [a[0]+b[0],a[1]+b[1],a[2]+b[2]]
+					})
+				}).reduce(function(a,b){
+				return [a[0]+b[0],a[1]+b[1],a[2]+b[2]]
+			});
+			imagejs.data.IrgbSeg=imagejs.data.IrgbSeg.map(function(x){return x/(imagejs.data.n*imagejs.data.density)});
+			imagejs.data.IrgbNonSeg=imagejs.data.IrgbNonSeg.map(function(x){return x/(imagejs.data.n*(1-imagejs.data.density))});
+			$('#'+divId+' .countShapesIntensity').html('RGB = ['+jmat.shorten(imagejs.data.IrgbSeg,5)+'], vs background: ['+jmat.shorten(jmat.innerfun(imagejs.data.IrgbSeg,imagejs.data.IrgbNonSeg,function(a,b){return a/b}),5)+']');
 			//imagejs.modules[id][divId].features = jmat.extractSegs(jmat.clone(jmat.edges(imagejs.data.seg)));
 			//$('#'+divId+' .countShapesFeatures').html(imagejs.modules[id][divId].features.length);
 		},
-		featuresTable:function(divId){
+		featuresStats:function(divId){
 			imagejs.data.segs = jmat.extractSegs(jmat.clone(imagejs.data.seg));
 			$('#'+divId+' .countShapesFeatures').html(imagejs.data.segs.length);
+			
 			
 		}
 		
